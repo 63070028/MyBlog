995689f835204196501989d68baff96d72894ea0
63070028 <63070028@kmitl.ac.th>
2023-05-25 00:34:51 +0700

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { PostsComponent } from './posts/posts.component';
+import { NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavbarComponent,
+    MyProfileComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbPaginationModule,
     NgbModule
   ],
   providers: [],
---
+++ src/app/blog.ts
@@ -0,0 +1,6 @@
+export interface Blog {
+    id:string,
+    title:string,
+    creation_date:string,
+    image_url:string,
+}
